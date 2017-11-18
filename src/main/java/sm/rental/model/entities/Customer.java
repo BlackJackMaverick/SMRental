b2bccb35f3263120c2df5d119a04e7094313115c
Junye <jyan006@uottawa.ca>
2017-11-18 15:37:16 -0500

    I added the classes for one of the entities structures

--- src/main/java/sm/rental/model/entities/Customer.java
+++ src/main/java/sm/rental/model/entities/Customer.java
@@ -1,4 +1,10 @@
 package sm.rental.model.entities;
 
 public class Customer {
+    protected double startTime; //The time at which a customer enters the system.
+    protected static enum CustomerType { N ,R }; //‘N’ for new customers or ‘R’ for returning customers
+    protected CustomerType uType;
+    protected int  numPassengers; //The number of passengers accompanying the customer plus the customer themselves.
+    protected static enum CustomerStatus { BOARDING, EXITING, WAITTING_PICKUP, WAITTING_SERVING, SERVING};
+    protected CustomerStatus status;
 }
