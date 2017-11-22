f948876c61f04a091757e9df908008b015201386
Junye <jyan006@uottawa.ca>
2017-11-22 15:25:34 -0500

    I made some changes for Customer.java

--- src/main/java/sm/rental/model/entities/Customer.java
+++ src/main/java/sm/rental/model/entities/Customer.java
@@ -1,10 +1,17 @@
 package sm.rental.model.entities;
 
 public class Customer {
-    protected double startTime; //The time at which a customer enters the system.
-    protected static enum CustomerType { N ,R }; //‘N’ for new customers or ‘R’ for returning customers
-    protected CustomerType uType;
-    protected int  numPassengers; //The number of passengers accompanying the customer plus the customer themselves.
-    protected static enum CustomerStatus { BOARDING, EXITING, WAITTING_PICKUP, WAITTING_SERVING, SERVING};
-    protected CustomerStatus status;
+    private double startTime; //The time at which a customer enters the system.
+    private static enum CustomerType { N ,R }; //‘N’ for new customers or ‘R’ for returning customers
+    private CustomerType uType;
+    public int  numPassengers; //The number of passengers accompanying the customer plus the customer themselves.
+
+
+    public Customer(double startT,CustomerType type,int numP ){
+       startTime = startT;
+       uType = type;
+       numPassengers = numP;
+    }
+    public Customer(){
+    };
 }
