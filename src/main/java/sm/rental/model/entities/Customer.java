63d61c6ece9185b385e6038fa74142d29b7b1061
Junye <jyan006@uottawa.ca>
2017-11-23 13:25:24 -0500

    Some changes for Customer.java

--- src/main/java/sm/rental/model/entities/Customer.java
+++ src/main/java/sm/rental/model/entities/Customer.java
@@ -2,7 +2,10 @@ package sm.rental.model.entities;
 
 public class Customer {
     private double startTime; //The time at which a customer enters the system.
-    private static enum CustomerType { N ,R }; //‘N’ for new customers or ‘R’ for returning customers
+    private static enum CustomerType {
+        N ,
+        R
+    };                           //‘N’ for new customers or ‘R’ for returning customers
     private CustomerType uType;
     public int  numPassengers; //The number of passengers accompanying the customer plus the customer themselves.
 
