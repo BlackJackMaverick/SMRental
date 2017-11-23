4b174b733ba6aa59838ba51235bc8924c9c6211f
Junye <jyan006@uottawa.ca>
2017-11-23 13:29:23 -0500

    I created Initialise.java

--- src/main/java/sm/rental/model/actions/Initialise.java
+++ src/main/java/sm/rental/model/actions/Initialise.java
@@ -1,7 +1,9 @@
 package sm.rental.model.actions;
 
+import simulationModelling.*;
 import simulationModelling.ScheduledAction;
 import sm.rental.model.SMRental;
+import sm.rental.model.entities.Van;
 
 public class Initialise extends ScheduledAction
 {
@@ -14,13 +16,28 @@ public class Initialise extends ScheduledAction
 	int tsix = 0;  // set index to first entry.
 	protected double timeSequence() 
 	{
+
 		return ts[tsix++];  // only invoked at t=0
 	}
 
 	protected void actionEvent() 
 	{
+		model.RentalLine.clear();
+		model.ReturnLine.clear();
+		model.Terminal1.clear();
+		model.Terminal2.clear();
+
+		int id;
+		for(id=1; id<=  ;id++) {
+			// Don't know the value of id.
+			model.rgVan[id].status= model.rgVan[id].status.LOADING_T1;
+			model.rgVan[id].capacity=model.rgVan[id].vanCapacity;
+			model.rgVan[id].n=0 ;
+			model.rgVan[id].mileage=0;
+		}
+
 		// System Initialisation
-                // Add initilisation instructions 
+		// Add initilisation instructions
 	}
 	
 
