7a149495a0251abb99664d79985aaaf337f7da81
Junye <jyan006@uottawa.ca>
2017-11-24 17:27:31 -0500

    Some changes for RentalCounter.java

--- src/main/java/sm/rental/model/entities/RentalCounter.java
+++ src/main/java/sm/rental/model/entities/RentalCounter.java
@@ -2,15 +2,13 @@ package  sm.rental.model.entities;
 
 public class RentalCounter {
 
-    private int unumAgentBusy; //The number of agents who are serving customers.
+    private int unumAgents; //The number of available agents.
     private double cost; // The total cost for all rental agents in the system during one time of experiment.
 
 
-    public RentalCounter(int numAgentBusy, double c) {
-        unumAgentBusy = numAgentBusy;
-        cost = c;
+    public RentalCounter(int unumAgents, double cost) {
+        this.unumAgents = unumAgents;
+        this.cost = cost;
     }
-    public RentalCounter(){
 
-    }
 }
