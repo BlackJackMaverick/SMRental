de429ef54b65ff7eb561cc9920bd07e54c01b060
Junye <jyan006@uottawa.ca>
2017-11-24 17:26:58 -0500

    Some changes for Van.java

--- src/main/java/sm/rental/model/entities/Van.java
+++ src/main/java/sm/rental/model/entities/Van.java
@@ -3,8 +3,7 @@ package  sm.rental.model.entities;
 import java.util.HashSet;
 
 public class Van {
-    Customer c=new Customer();
-    public static enum VansStatus {
+    private static enum VansStatus {
                         BOARDING_T1,
                         BOARDING_T2,
                         BOARDING_RC,
@@ -21,36 +20,40 @@ public class Van {
                         UNLOADING_RC,
                         UNLOADING_DP
     };
-    public VansStatus status;
+    private VansStatus status;
     public HashSet<Customer> group = new HashSet<Customer>(); //List of customers currently in each van
-    public int capacity;//Number of seats left available.
-    public double mileage;//Total number of miles driven by the van in the observation interval
-    public int n=getN();
-    public int vanCapacity=12; // OR 18,30
+    private int capacity;//Number of seats left available.
+    private double mileage;//Total number of miles driven by the van in the observation interval
+    private int numVans; //Total number of vans running in the system.
 
-
-
+    public int getNumVans(){
+        return this.numVans;
+    }
+    public VansStatus getVansStatus(){
+        return this.status;
+    }
+    public void setVansStatus(VansStatus status){
+        this.status=status;
+    }
+    public Van getVans(){
+        return Van.this;
+    }
     // Required methods to manipulate the group
     public void insertGrp(Customer icgCustomer) {
         group.add(icgCustomer);
-        capacity -= c.numPassengers;
+        capacity -= icgCustomer.getPassengers();
     }
     public boolean removeGrp(Customer icgCustomer) {
-        capacity += c.numPassengers;
+        capacity += icgCustomer.getPassengers();
         return group.remove(icgCustomer);
-
     }
     public int getN() {
         return group.size();
     }// Attribute n
 
-
-    public Van(VansStatus sta, int cap, double mil){
-         status = sta;
-         capacity = cap;
-         mileage = mil;
+    public Van(int capacity){
+         this.capacity = capacity;
 
     }
-    public Van(){
-    }
+
 }
