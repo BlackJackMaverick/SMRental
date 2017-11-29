bebb194000725748c1cee862039e0011e3454097
Junye <jyan006@uottawa.ca>
2017-11-29 13:11:31 -0500

    Create CustomerArrivalT1

---
+++ src/main/java/sm/rental/model/actions/NewCustomerArrivalT1.java
@@ -0,0 +1,23 @@
+package sm.rental.model.actions;
+
+import simulationModelling.ScheduledAction;
+
+class NewCustomerArrivalT1 extends ScheduledAction{
+    SMRental model;
+    public NewCustomerArrivalT1(SMRental model){
+        this.model=model;
+    }
+
+    public double timeSequence(){
+        return model.rvp.DuNCustomerT1();
+    }
+
+    public void actionEvent(){
+        double startTime = model.getClock();
+        CustomerType uType = model.NEW;
+        int numPassengers = model.rvp.uNumPassengers();
+
+        Customer icgCustomer= new Customer( startTime, uType, numPassengers );
+        model.Terminal1.spInsertQue(icgCustomer);
+    }
+}
