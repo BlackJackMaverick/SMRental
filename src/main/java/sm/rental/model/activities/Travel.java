35acea55a135a5bead4439938737d2b2dda0a35c
Junye <jyan006@uottawa.ca>
2017-12-01 21:22:30 -0500

    I created travel.java

--- src/main/java/sm/rental/model/activities/Travel.java
+++ src/main/java/sm/rental/model/activities/Travel.java
@@ -1,4 +1,55 @@
 package sm.rental.model.activities;
 
-public class Travel {
+import lombok.Getter;
+import simulationModelling.ConditionalActivity;
+import sm.rental.model.SMRental;
+import sm.rental.model.entities.Van;
+import sm.rental.model.procedures.DVPs;
+import sm.rental.model.procedures.RVPs;
+import sm.rental.model.procedures.UDPs;
+
+public class Travel extends ConditionalActivity {
+    @Getter SMRental model;
+    UDPs udp;
+    Van rgVan;
+    DVPs dvp;
+
+    public Travel(SMRental model){ this.model = model; }
+    public static boolean precondition(SMRental model){
+        boolean returnValue = false;
+
+        if (model.udp.CanUnloadVan() == true){
+
+            returnValue = true;
+        };
+        return returnValue;
+    }
+    public void startingEvent() {
+        rgVan = this.model.udp.GetVanForTravel();
+        VanLocation nextDestination = this.model.udp.GetNextDestinationForVan(rgVan);
+        this.model.udp.UpdateVanStatus( rgVan, "TRAVELLING");
+    }
+    protected double duration(){
+
+        return (this.model.dvp.travelTime( rgVan,nextDestination ));
+
+    }
+    public void terminatingEvent(){
+        this.model.udp.UpdateVanLocation( rgVan,nextDestination);
+        if( nextDestination.equals("RENTAL_COUNTER" || nextDestination.equals("DROP_OFF"))){
+            if( rgVan.getSeatsAvailable() != rgVan.getCapacity()){
+                this.model.udp.UpdateVanStatus( rgVan,"UNLOADING");
+            }
+
+        }
+        else{
+            this.model.udp.UpdateVanStatus( rgVan,"UNLOADING");
+        }
+    }
+
+
+
+
+
+
 }
