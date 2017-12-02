18dc11e2c66904948e1c1e6ad212c67211e29dcf
aklef <andreas@klef.ca>
2017-12-01 20:09:44 -0500

    Boilerplate commit

--- src/main/java/sm/rental/model/activities/Travel.java
+++ src/main/java/sm/rental/model/activities/Travel.java
@@ -1,4 +1,37 @@
 package sm.rental.model.activities;
 
-public class Travel {
+import simulationModelling.ConditionalActivity;
+import sm.rental.model.SMRental;
+
+public class Travel extends ConditionalActivity
+   {
+   private SMRental model;
+//   private Location origin;
+//   private Location destination;
+   private int vanId;
+
+   public Travel(SMRental model) {
+      this.model = model;
+   }
+
+   public static boolean precondition(SMRental model)
+   {
+      return false;
+   }
+
+   @Override
+   public void startingEvent()
+   {
+   }
+
+   @Override
+   protected double duration()
+   {
+      return 0.0;
+   }
+
+   @Override
+   protected void terminatingEvent()
+   {
+   }
 }
